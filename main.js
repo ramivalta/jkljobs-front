f99e393668972f49194fa15b46d5eb98d5baa627
Rami Valta <rami.valta@gmail.com>
2014-06-15 12:03:20 +0300

    Implement naive search feature, show source links for all supported sites

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ function viewModel() {
 	var self = this;
 	self.jobs = ko.observableArray();
 	self.displayedItem = ko.observable();
+	self.search = ko.observable("");
+	var jobs = [];
 
 	$.get(
 		'http://jkljobs.herokuapp.com/api/v1/jobs',
@@ -13,6 +15,7 @@ function viewModel() {
 				if(a.added < b.added) return 1;
 				else return 0;
 			});
+			jobs = data;
 			ko.mapping.fromJS(data, {}, self.jobs);
 			self.showItem(0);
 		}
@@ -31,6 +34,24 @@ function viewModel() {
 	self.showItem = function(idx) {
 		self.displayedItem(self.jobs()[idx]);
 	}
+
+	self.searchJobs = ko.computed(function() {
+		search = self.search().trim().split(" ");
+
+		var filt = jobs.filter(function(item) {
+			for (var i = 0; i < search.length; i++) {
+				var itemdata = item.title.toLowerCase() + " " + item.company.toLowerCase();
+				if (_.contains(itemdata, search[i].toLowerCase()))
+					return item;
+			}
+		});	
+
+		/*self.jobs([]);
+		self.jobs.push.apply(self.jobs, filt);*/
+
+		ko.mapping.fromJS(filt, {}, self.jobs);
+
+	}).extend({ rateLimit: { method: "notifyWhenChangesStop", timeout: 250 }});
 
 }
 
